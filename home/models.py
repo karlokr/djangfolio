690b2e4fa35fbe162053b87c19b87c627433eb05
karlokr-p <karlo.krakan@panevo.com>
2026-02-05 21:39:52 -0700

    feat: Copyright notice improvements

--- home/models.py
+++ home/models.py
@@ -49,8 +49,8 @@ class SiteConfiguration(models.Model):
     # Copyright Information
     copyright_text = models.CharField(
         max_length=200,
-        default='Your Name. All rights reserved',
-        help_text='Copyright text displayed in footer'
+        default='Your Name. All rights reserved.',
+        help_text='Copyright text displayed in footer (year is added automatically)'
     )
     
     # Profile Image
